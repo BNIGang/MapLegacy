40bfa26b93659d03b0d99b53c99e5ac34350cc47
Derpen <rizqyassabil@hotmail.com>
2023-04-12 08:31:57 +0700

    Login capability acquired, not connected to db yet

--- cmd/api/main/main.go
+++ cmd/api/main/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"github.com/BNIGang/MapLegacy/login"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func main() {
+	engine := html.New("./web/template", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	// Login page
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("./web/static/login.html")
+	})
+
+	app.Post("/login", login.Handler)
+
+	app.Get("/home", func(c *fiber.Ctx) error {
+		return c.Render("home", fiber.Map{})
+	})
+
+	port := ":8000"
+	app.Listen(port)
+}
--- login/login.go
+++ login/login.go
@@ -1,1 +1,25 @@
 package login
+
+import "github.com/gofiber/fiber/v2"
+
+func Handler(c *fiber.Ctx) error {
+	// Check if the username and password are correct (use placeholder values for now)
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	if username == "user" && password == "pass" {
+		// Set a cookie to indicate that the user is logged in
+		cookie := &fiber.Cookie{
+			Name:  "token",
+			Value: "jwt-token",
+			Path:  "/",
+		}
+		c.Cookie(cookie)
+
+		// Redirect to the home page
+		return c.Redirect("/home", fiber.StatusSeeOther)
+	}
+
+	// If the username and password are incorrect, render the login page again with an error message
+	return c.Render("login", fiber.Map{"Error": "Incorrect username or password"})
+}
