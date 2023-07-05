40979dd10cd5c6aff5c049618325e71d9b98e461
Derpen <rizqyassabil@hotmail.com>
2023-07-05 11:27:09 +0700

    Preparations again

--- cmd/api/main/main.go
+++ cmd/api/main/main.go
@@ -6,7 +6,6 @@ import (
 	u "github.com/BNIGang/MapLegacy/api/v1/user"
 	"github.com/BNIGang/MapLegacy/login"
 	"github.com/BNIGang/MapLegacy/web"
-	"github.com/derpen/fastergoding"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
 )
@@ -17,7 +16,7 @@ var secret []byte = login.Secret
 
 func main() {
 
-	fastergoding.Run("./cmd/api/main")
+	// fastergoding.Run("./cmd/api/main")
 
 	engine := html.New("./web/template", ".html")
 
